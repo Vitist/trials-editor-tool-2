9137d49d4ca568d790d7395374159e584c1f9dfa
Teemu Tuovinen <teemu.tuovinen@eng.tamk.fi>
2019-01-30 16:00:34 +0200

    Simplified config initialization

--- TrialsEditorTool/config.cpp
+++ TrialsEditorTool/config.cpp
@@ -2,6 +2,7 @@
 #include <QFile>
 #include <QDebug>
 #include <QRegularExpression>
+#include <QMap>
 
 Config::Config(): userId("")
 {
@@ -13,22 +14,27 @@ bool Config::initialize(QDir saveDir)
     if(!load()){
         // Editor tracks end with -0000000000000
         QFileInfoList editorTrackDirectories = saveDir.entryInfoList(QStringList() << "*-0000000000000", QDir::Dirs | QDir::NoDotAndDotDot);
+
         if(!editorTrackDirectories.isEmpty()) {
-            QFileInfo editorTrack = editorTrackDirectories.first();
-            // Read user id and platform from a track file
-            QFile file(editorTrack.filePath() + QDir::separator() + "track.trk");
-            if(file.open(QIODevice::ReadOnly)) {
-                qDebug() << "Initializing config from track file";
-                QByteArray content = file.readAll();
-                userId = content.mid(9, 16).toHex();
-                qDebug() << "userId: " << userId;
-                save();
+            // User might have someone elses editor track in the SaveGames directory
+            // Count each user id found from editor track directory names
+            QMap<QString, int> userIdCount;
+            foreach(QFileInfo track, editorTrackDirectories) {
+                QString id = track.fileName().left(32);
+                userIdCount[id]++;
             }
-            else {
-                qDebug() << "Couldn't open track file for config initialization";
-                success = false;
+
+            // Set the user id to be the one that was repeated the most
+            // User id will be wrong if there are more editor tracks from someone else than the user
+            int count = 0;
+            foreach(QString id, userIdCount.keys()) {
+                if(userIdCount[id] > count) {
+                    userId = id;
+                }
             }
-            file.close();
+
+            qDebug() << userIdCount;
+            qDebug() << "Config initialized with user id: " << userId;
         }
         else {
             qDebug() << "No editor tracks found for config initialization";
